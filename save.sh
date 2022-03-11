4d4da225bbfa7c63207490063c429a3384fd3935
meulon <marlon@ik.me>
2022-03-11 21:40:33 +0100

    save v1

---
+++ save.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Plusieurs paramètres
+# nom archive
+# nom du repertoire à sauvegarder
+# adresse du serveur de sauvegarde
+# login et password du compte
+# archive réalisée avec tar
+# copie à travers du réseau de l'archive
+
+tar -cvf $1 $2
