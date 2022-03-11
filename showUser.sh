ec3bcd43c9a1c306eb391947160023ea9815e1ef
meulon <marlon@ik.me>
2022-03-11 21:29:34 +0100

    showUser final

--- showUser.sh
+++ showUser.sh
@@ -6,11 +6,12 @@
 # l'id de l'utilisateur
 # des groupes de l'utilisateur
 
-
 ID=$(cat /etc/passwd | grep $USER | cut -d ':' -f 3)
 GROUP=$(groups $USER | cut -d ' ' -f 3,4)
+TIMESTAMP=$(date | cut -d ' ' -f 4)
+FILENAME=$USER\_$TIMESTAMP.txt
 
-echo Vous êtes : $USER
-echo Avec ID : $ID
-echo Present dans les groups : $GROUP
+echo Vous êtes : $USER | tee $FILENAME
+echo Avec ID : $ID | tee -a $FILENAME
+echo Present dans les groups : $GROUP | tee -a $FILENAME
 exit 0
