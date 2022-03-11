fd86be8fca9c03a6e916e8aee2ed8368b2fa4c8f
meulon <marlon@ik.me>
2022-03-11 21:49:47 +0100

    save add scp

--- save.sh
+++ save.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # Plusieurs paramètres
-# nom archive
-# nom du repertoire à sauvegarder
-# adresse du serveur de sauvegarde
-# login et password du compte
+# nom archive ($1)
+# nom du repertoire à sauvegarder ($2)
+# adresse du serveur de sauvegarde + chemin : adresse:chemin ($4) 
+# login et password du compte ($3)
 # archive réalisée avec tar
 # copie à travers du réseau de l'archive
 
 tar -cvf $1 $2
+scp $1 $3:$4@$5
+
+exit 0
