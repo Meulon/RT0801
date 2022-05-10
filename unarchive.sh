eb21d96685ceb2be16fd46e3fca3414184fd9f4e
Marlon <marlon@ik.me>
2022-05-10 15:01:24 +0000

    fix

---
+++ unarchive.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Description du script
+# • Paramètres
+# o le nom de l’archive
+# o le répertoire de base d’installation 
+# • Opérations
+# o Vérification des existences et des droits
+# o Test du type d’archive (zip, tar, tgz) (Uniquement sur l’extension) 
+# o Utilisation de l’archiveur correspondant
