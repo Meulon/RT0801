8537429109ff7d5edbf05c36d4d1e3e8e7b92b35
Marlon <marlon@ik.me>
2022-05-12 06:39:14 +0000

    fix

--- unarchive.sh
+++ unarchive.sh
@@ -16,10 +16,20 @@ echo $groupUser
 ownerArchive=$(stat -L -c "%U" $archive)
 groupOwnerArchive=$(stat -L -c "%G" $archive)
 accessRights=$(stat -L -c "%a" $archive)
+echo $accessRights
+ownerAccessRights=${accessRights:0:1}
+groupAccessRights=${accessRights:1:1}
+othersAccessRights=${accessRights:2:1}
+echo $ownerAccessRights
+echo $groupAccessRights
+echo $othersAccessRights
 #ownerArchive=$(stat -L -c "%a %G %U" $archive)
-if [[ $ownerArchive == $USER || $groupOwnerArchive == *"$groupUser"* ]]; then
-	if [[ $accessRights == 6** ]]; then
-		echo yes
-	fi
+if [[ $ownerArchive == $USER &&	$ownerAccessRights -ge 4 ]]; then
 	tar xvf $archive --directory=$destinationDirectory
+elif [[ $groupOwnerArchive == *"$groupUser"* && $groupAccessRights -ge 4 ]]; then
+	tar xvf $archive --directory=$destinationDirectory
+elif [[ $othersAccessRights -ge 4 ]]; then
+	tar xvf $archive --directory=$destinationDirectory
+else
+	echo no permission to unarchive
 fi
