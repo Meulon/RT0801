e8003f3dbc6a47c0d97847e55d728b3470ab84f0
Marlon <marlon@ik.me>
2022-05-24 15:57:15 +0000

    creat, destroy container init

---
+++ container/2/creat_container.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+# Assure la création d'un conteneur LXC
+# Ce script prendra en paramètre l'ensemble des paramètres du conteneur : template, adresse réseau, configuration système (mémoire, CPU ...), mot de passe root
+# Ce script assurera la modification du fichier de configuration du conteneur
---
+++ container/2/del_container.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+# Assure la destruction d'un ou de plusieurs conteneurs dont les identifiants sont passés en paramètres
+# Ce script permettra l'utilisation de jokers : cnt* désignera l'ensemble des conteneurs dont le nom commence par cnt
+# Vous pourrez étendre à l'utilisation des expressions régulières pour désigner les conteneurs
